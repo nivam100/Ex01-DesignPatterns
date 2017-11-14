c032c51c2e961886f621f12eb86eb06845a9f6ca
FooTaGe <pookair@gmail.com>
2017-11-15 00:00:35 +0200

    .net 4, basic login and logout.

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.pictureBoxProfile = new System.Windows.Forms.PictureBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(2, 12);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 0;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Location = new System.Drawing.Point(2, 41);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonLogout.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogout.TabIndex = 1;
+            this.buttonLogout.Text = "Logout";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // pictureBoxProfile
+            // 
+            this.pictureBoxProfile.Location = new System.Drawing.Point(84, 12);
+            this.pictureBoxProfile.Name = "pictureBoxProfile";
+            this.pictureBoxProfile.Size = new System.Drawing.Size(57, 50);
+            this.pictureBoxProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxProfile.TabIndex = 2;
+            this.pictureBoxProfile.TabStop = false;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(12, 67);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(55, 13);
+            this.labelUsername.TabIndex = 3;
+            this.labelUsername.Text = "Username";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 381);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.pictureBoxProfile);
+            this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.buttonLogin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonLogout;
+        private System.Windows.Forms.PictureBox pictureBoxProfile;
+        private System.Windows.Forms.Label labelUsername;
     }
 }
 
--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +15,99 @@ namespace WindowsFormsApp
         public Form1()
         {
             InitializeComponent();
+            FacebookWrapper.FacebookService.s_CollectionLimit = 200;
+            FacebookWrapper.FacebookService.s_FbApiVersion = 2.8f;
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            loginAndInit();
+        }
+        User m_LoggedInUser;
+
+        private void loginAndInit()
+        {
+            /// Owner: design.patterns
+
+            /// Use the FacebookService.Login method to display the login form to any user who wish to use this application.
+            /// You can then save the result.AccessToken for future auto-connect to this user:
+            LoginResult result = FacebookService.Login("124318548263284", /// (design patterrns Ex01 app)
+                "public_profile",
+                "user_education_history",
+                "user_birthday",
+                "user_actions.video",
+                "user_actions.news",
+                "user_actions.music",
+                "user_actions.fitness",
+                "user_actions.books",
+                "user_about_me",
+                "user_friends",
+                "publish_actions",
+                "user_events",
+                "user_games_activity",
+                //"user_groups" (This permission is only available for apps using Graph API version v2.3 or older.)
+                "user_hometown",
+                "user_likes",
+                "user_location",
+                "user_managed_groups",
+                "user_photos",
+                "user_posts",
+                "user_relationships",
+                "user_relationship_details",
+                "user_religion_politics",
+
+                //"user_status" (This permission is only available for apps using Graph API version v2.3 or older.)
+                "user_tagged_places",
+                "user_videos",
+                "user_website",
+                "user_work_history",
+                "read_custom_friendlists",
+
+                // "read_mailbox", (This permission is only available for apps using Graph API version v2.3 or older.)
+                "read_page_mailboxes",
+                // "read_stream", (This permission is only available for apps using Graph API version v2.3 or older.)
+                // "manage_notifications", (This permission is only available for apps using Graph API version v2.3 or older.)
+                "manage_pages",
+                "publish_pages",
+                "publish_actions",
+
+                "rsvp_event"
+                );
+           
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                m_LoggedInUser = result.LoggedInUser;
+                fetchUserInfo();
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorMessage);
+            }
+        }
+
+        private void fetchUserInfo()
+        {
+            string FirstAndLastname = String.Format("{0} {1}", this.m_LoggedInUser.FirstName, m_LoggedInUser.LastName);
+            this.labelUsername.Text = FirstAndLastname;
+            this.pictureBoxProfile.ImageLocation = this.m_LoggedInUser.PictureSmallURL;
+            
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            Action logoutMessage = OnSuccessfullLogout;
+            FacebookService.Logout(logoutMessage);
+        }
+
+        private static void OnSuccessfullLogout()
+        {
+            MessageBox.Show("Successfull Logout");
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            loginAndInit();
         }
     }
 }
